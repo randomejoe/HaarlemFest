ffe9572c00f534c26bbd4a7ed0ba24724019fa72
Sem-Philippo <144238995+Sem-Philippo@users.noreply.github.com>
2026-02-10 14:58:55 +0100

    change CMS nav tabs to start with capital letter

--- app/public/cms/components/nav.php
+++ app/public/cms/components/nav.php
@@ -1,21 +1,21 @@
 <link rel="stylesheet" href="../css/style.css">
 <nav class='nav'><div class='navItemsContainer'>
     <a href="index.php" class='navItem'>
-        <p>dashboard</p>
+        <p>Dashboard</p>
     </a>
     <a href="pages.php" class='navItem'>
-        <p>pages</p>
+        <p>Pages</p>
     </a>
     <a href="components.php" class='navItem'>
-        <p>components</p>
+        <p>Components</p>
     </a>
     <a href="events.php" class='navItem'>
-        <p>events</p>
+        <p>Events</p>
     </a>
     <a href="tickets.php" class='navItem'>
-        <p>tickets</p>
+        <p>Tickets</p>
     </a>
     <a href="users.php" class='navItem'>
-        <p>users</p>
+        <p>Users</p>
     </a>
 </div></nav>
