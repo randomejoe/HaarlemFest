ef91d8eb76300429acc6d30aca06ab2f5032d711
Sem-Philippo <144238995+Sem-Philippo@users.noreply.github.com>
2026-03-09 15:02:30 +0100

    fix page content appearing with no content

--- app/src/Views/partials/cms/edit_pages.php
+++ app/src/Views/partials/cms/edit_pages.php
@@ -5,9 +5,11 @@
     $itemType = "content";
     foreach ($item as $contentItem)
     {
-        $itemName = $contentItem['component_name'];
+        if (isset($contentItem['component_name'])) {
+            $itemName = $contentItem['component_name'];
         $itemId = $contentItem['content_id'];
         require __DIR__ . '/cms_item.php';
+        }        
     }
     require __DIR__ . '/add_page_content.php'
     ?>
