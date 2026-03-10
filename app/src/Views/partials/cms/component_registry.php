315a0d1c10f71b1038dea8eb92ab02b85434673c
Sem-Philippo <144238995+Sem-Philippo@users.noreply.github.com>
2026-03-10 11:59:00 +0100

    Add comment to component_registry.php to explain the fields

--- app/src/Views/partials/cms/component_registry.php
+++ app/src/Views/partials/cms/component_registry.php
@@ -1,3 +1,8 @@
+<!-- Name is the name of the field, visible when editing values in the cms and what is used to get the value through $data 
+(ex. 'name'=>'count' is accessed as $data['count']) 
+type refers to the type of edit field, types are image and text. More can be added on request
+element is only for text elements currently and refers to the element used to wrap the text in the wysiwyg editor
+-->
 <?php $components = [
     'text' => [
         'fields' => [
---
+++ app/src/Views/partials/page_components/title.php
@@ -0,0 +1,1 @@
+<h2 class="wysiwyg"><?php echo $data['title_text'] ?? '' ?></h2>
