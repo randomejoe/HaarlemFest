49eb88ed46425c3530d4197b1196f2f83ed5dd4c
Sem-Philippo <144238995+Sem-Philippo@users.noreply.github.com>
2026-03-24 11:22:40 +0100

    change cms events view to conform to ticket selling system for availability show

--- app/src/Views/partials/cms/cms_item_event_fields.php
+++ app/src/Views/partials/cms/cms_item_event_fields.php
@@ -1,6 +1,6 @@
 <div class="row half-width between">
     <p><?php echo $item->location(); ?></p>
     <p><?php echo '€' . $item->ticketPrice(); ?></p>
-    <p><?php echo ($item->ticketAmount()-$item->getSoldTickets()) . '/' . $item->ticketAmount(); ?></p>
+    <p><?php echo $item->ticketAmount() . '/' . ($item->ticketAmount()+$item->getSoldTickets()); ?></p>
     <p><?php echo urldecode($item->category()); ?></p>
 </div>
