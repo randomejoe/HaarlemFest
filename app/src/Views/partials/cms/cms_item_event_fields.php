acf9c68112187368114359bbca4699ec63380c57
Sem-Philippo <144238995+Sem-Philippo@users.noreply.github.com>
2026-03-24 11:25:09 +0100

    changes to showing free events in cms

--- app/src/Views/partials/cms/cms_item_event_fields.php
+++ app/src/Views/partials/cms/cms_item_event_fields.php
@@ -1,6 +1,6 @@
 <div class="row half-width between">
     <p><?php echo $item->location(); ?></p>
-    <p><?php echo '€' . $item->ticketPrice(); ?></p>
-    <p><?php echo $item->ticketAmount() . '/' . ($item->ticketAmount()+$item->getSoldTickets()); ?></p>
+    <p><?php if ($item->ticketPrice() > 0) {echo '€' . $item->ticketPrice();} else {echo 'Free';} ?></p>
+    <p><?php if ($item->ticketAmount() !== null) {echo $item->ticketAmount() . '/' . ($item->ticketAmount()+$item->getSoldTickets());} ?></p>
     <p><?php echo urldecode($item->category()); ?></p>
 </div>
