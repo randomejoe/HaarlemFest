f96a6fbd08019343d2c581e3aa0d8e4275b3b752
Sem-Philippo <144238995+Sem-Philippo@users.noreply.github.com>
2026-04-07 14:36:02 +0200

    fix history main page styling for small screens

--- app/src/Views/partials/page_components/component_partials/history_locations_card.php
+++ app/src/Views/partials/page_components/component_partials/history_locations_card.php
@@ -1,10 +1,14 @@
 <div class="history-location-card d-flex justify-content-between <?php echo $index % 2 == 0 ? 'flex-row' : 'flex-row-reverse'?>">
-    <div class="d-flex <?php echo $index % 2 == 0 ? 'flex-row' : 'flex-row-reverse flex-grow-1 justify-content-between'?>">
-        <img src='<?php echo '/images/' . $location->getImage() ?>' width="200">
-        <div class="p-4">
-            <h3> <?php echo $location->getName() ?></h3>
-            <?php echo $location->getDescription(); ?>
+    <div class="d-flex <?php echo $index % 2 == 0 ? 'left' : 'right flex-grow-1 justify-content-between'?>">
+        <img src='<?php echo '/images/' . $location->getImage() ?>'>
+        <div class="px-4">
+            <div>
+                <h3> <?php echo $location->getName() ?></h3>
+                <?php echo $location->getDescription(); ?>
+            </div>
+            <div class="d-flex history-location-card-learn-more <?php echo $index % 2 == 0 ? 'right' : 'left'?>">
+                <a href="/<?php echo $location->getName(); ?>">Learn more</a>
+            </div>
         </div>
     </div>
-    <a href="/<?php echo $location->getName(); ?>">Learn more</a>
 </div>
--- app/src/Views/partials/page_components/component_partials/history_schedule_card_container.php
+++ app/src/Views/partials/page_components/component_partials/history_schedule_card_container.php
@@ -1,5 +1,5 @@
 <div class="history-schedule-day"> <?php 
-?><div class="horizontal-center"><h3><?php echo date('F jS', strtotime($date)); ?></h3></div>
+?><div class="horizontal-center"><h3 class="my-2"><?php echo date('F jS', strtotime($date)); ?></h3></div>
 <div class="history-schedule-card-container"><?php
 foreach ($scheduleDay as $time => $scheduleTime) {
     require __DIR__ . '/history_schedule_card.php';
