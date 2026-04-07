eb15c31b4d7f2da81891b2a088c99d32943d4680
Sem-Philippo <144238995+Sem-Philippo@users.noreply.github.com>
2026-04-07 14:58:36 +0200

    fix history detailpage styling for small screens

--- app/src/Views/partials/page_components/double_caption_image.php
+++ app/src/Views/partials/page_components/double_caption_image.php
@@ -1,9 +1,9 @@
-<div class="d-flex flex-row gap-4 justify-content-evenly <?php if (isset($data['has_horizontal_padding']) && $data['has_horizontal_padding'] == "1") {echo 'padding-horizontal ';} ?>">
-    <div class="w-40 d-flex flex-column justify-content-center">
+<div class="d-flex justify-content-evenly double-caption-image <?php if (isset($data['has_horizontal_padding']) && $data['has_horizontal_padding'] == "1") {echo 'padding-horizontal ';} ?>">
+    <div class="d-flex flex-column justify-content-center">
         <img src="/images/<?php echo $data['left_image_source']; ?>">
         <p class="text-center"><?php echo $data['left_image_caption']; ?></p>
     </div>
-    <div class="w-40 d-flex flex-column justify-content-center">
+    <div class="d-flex flex-column justify-content-center">
         <img src="/images/<?php echo $data['right_image_source']; ?>">
         <p class="text-center"><?php echo $data['right_image_caption']; ?></p>
     </div>
