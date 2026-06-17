270326a61cdf3aa95fcda3dcecbd67b833fc642a
Sem-Philippo <144238995+Sem-Philippo@users.noreply.github.com>
2026-06-18 00:01:44 +0200

    change family ticket quantity from a magic number to a const

--- app/src/Services/PlannerService.php
+++ app/src/Services/PlannerService.php
@@ -14,6 +14,7 @@ use RuntimeException;
 class PlannerService implements IPlannerService
 {
     private const FAMILY_TICKET_PRICE = 60;
+    private const PARTICIPANTS_PER_FAMILY_TICKET = 4;
 
     private IEventRepository $events;
     private SessionManager $session;
@@ -254,7 +255,7 @@ class PlannerService implements IPlannerService
     private function calculateLineTotal(int $quantity, Event $event, bool $familyTicket): float
     {
         if ($familyTicket) {
-            return (float) (self::FAMILY_TICKET_PRICE * ceil($quantity / 4));
+            return (float) (self::FAMILY_TICKET_PRICE * ceil($quantity / PARTICIPANTS_PER_FAMILY_TICKET));
         }
 
         return $event->ticketPrice() * $quantity;
